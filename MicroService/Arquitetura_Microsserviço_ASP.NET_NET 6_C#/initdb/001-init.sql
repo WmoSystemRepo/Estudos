0ae5e2b059fa26b888abeae73f991684b3edf39f
Mendes <mendes@keltecnologia.com.br>
2025-09-05 18:36:37 -0300

    Iniciando a branch

---
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/initdb/001-init.sql
@@ -0,0 +1,4 @@
+﻿-- Exemplo simples para validar inicialização:
+CREATE TABLE IF NOT EXISTS healthcheck (id INT PRIMARY KEY, ok TINYINT);
+INSERT INTO healthcheck (id, ok) VALUES (1,1)
+  ON DUPLICATE KEY UPDATE ok=VALUES(ok);
