30393d8dfb08f7826349c1bfdb8d8985db65ba2f
Mendes <mendes@keltecnologia.com.br>
2025-09-09 13:49:02 -0300

    Aula 78

--- MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<IEnumerable<ProductVO>> FindById(long id);
+        Task<IEnumerable<ProductVO>> Create(ProductVO vo);
+        Task<IEnumerable<ProductVO>> Update(ProductVO vo);
+        Task<IEnumerable<ProductVO>> Delete(long id);
     }
 }
