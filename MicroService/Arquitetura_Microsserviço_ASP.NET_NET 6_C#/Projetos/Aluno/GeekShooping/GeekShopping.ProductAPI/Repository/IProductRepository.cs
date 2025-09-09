76a50fea5a26752e2cb998ffc1cb7c8117054b0b
Mendes <mendes@keltecnologia.com.br>
2025-09-09 15:40:16 -0300

    Aula 81

--- MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace GeekShopping.ProductAPI.Repository
     public interface IProductRepository
     {
         Task<IEnumerable<ProductVO>> FindAll();
-        Task<IEnumerable<ProductVO>> FindById(long id);
+        Task<ProductVO> FindById(long id);
         Task<IEnumerable<ProductVO>> Create(ProductVO vo);
         Task<IEnumerable<ProductVO>> Update(ProductVO vo);
         Task<IEnumerable<ProductVO>> Delete(long id);
---
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySqlContext _context;
+        private IMapper _mapper;
+
+        public ProductRepository(MySqlContext context, IMapper mapper )
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<IEnumerable<ProductVO>>( products );
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            Product products = 
+                await _context.Products.Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
+
+            return _mapper.Map<ProductVO>(products);
+        }
+
+        public Task<IEnumerable<ProductVO>> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+                public Task<IEnumerable<ProductVO>> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductVO>> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }    
+    }
+}
