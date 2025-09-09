9a137131cc4cb3a23b715a36eba4fa4613c8831e
Mendes <mendes@keltecnologia.com.br>
2025-09-09 15:23:39 -0300

    Aula 79

--- MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,14 +1,23 @@
-﻿using GeekShopping.ProductAPI.Model.Context;
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection string (conforme seu appsettings.json)
+// String de conexão MySQL (ajuste conforme seu appsettings.json)
 var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
+// Configura o DbContext
 builder.Services.AddDbContext<MySqlContext>(options =>
     options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
+// Configuração e registro do AutoMapper
+var mappingConfig = MappingConfig.RegisterMaps();
+IMapper mapper = mappingConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+// Controllers e documentação Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,5 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
