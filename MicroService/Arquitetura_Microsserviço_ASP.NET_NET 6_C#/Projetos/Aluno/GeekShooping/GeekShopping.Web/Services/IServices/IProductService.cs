69242280f9a318d942dfec4f4c9fbf86be37967e
Mendes <mendes@keltecnologia.com.br>
2025-09-09 22:05:25 -0300

    Sc_9_ A90

---
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> Update(ProductModel model);
+        Task<bool> DeleteProcductById(long id);
+    }
+}
