c6c1e0fc74e53056c9e376fd3ab358d3330b886a
Mendes <mendes@keltecnologia.com.br>
2025-09-09 22:19:37 -0300

    Sc_9_ A91

---
+++ MicroService/Arquitetura_Microsserviço_ASP.NET_NET 6_C#/Projetos/Aluno/GeekShooping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Algo deu errado! Erro: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
